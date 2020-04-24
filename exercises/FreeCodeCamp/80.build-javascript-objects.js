6ddd14b3e8984bc4d353c5d4c5ba55b72845a0e1
Nathan Neelis <55492381+NathanNeelis@users.noreply.github.com>
2020-04-24 14:27:36 +0200

    Add files via upload

---
+++ exercises/FreeCodeCamp/80.build-javascript-objects.js
@@ -0,0 +1,9 @@
+var myDog = {
+// Only change code below this line
+name: "Nathan",
+legs: 4,
+tails: 1,
+friends: ["Bello", "Vicky"]
+
+// Only change code above this line
+};
---
+++ exercises/FreeCodeCamp/89.manipulating-complex-objects.js
@@ -0,0 +1,26 @@
+var myMusic = [
+  {
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": true
+  },
+  // Add a record here
+  {
+    "artist": "Mario",
+    "title": "gitar Man",
+    "release_year": 1890,
+    "formats": [
+      "CD",
+      "mp3",
+      "LP"
+    ],
+    "gold": false
+
+  }
+];
